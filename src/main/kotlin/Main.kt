976b791c4f7db36c4ae213ef687d2dfe295ab4a6
login <mail@yourmail.ru>
2023-12-15 18:18:54 +0200

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,23 +5,29 @@ fun main(){
     base.info()
     val base1 = Base(1, 9)
     base1.info()
+    val child = Child(3, 2, 3f)
+    child.childInfo()
 }
 
-class Base(number1:Int, number2:Int){
-    val number1:Int
-    val number2:Int
-    var answer: Double = 0.0
-
-    init {
-        this.number1 = number1
-        this.number2 = number2
-    }
+open class Base(
+    val number1:Int,
+    val number2:Int){
 
     fun info(){
-        answer = sqrt((number1.toDouble()* number2.toDouble()))
+        val answer = sqrt((number1.toDouble()* number2.toDouble()))
         println("√($number1 * $number2) = $answer")
     }
 }
 
+class Child(
+    number1:Int,
+    number2:Int,
+    val fNumber: Float) : Base(number1, number2){
 
+   fun childInfo(){
+       val answer = sqrt((number1.toFloat()* number2.toFloat() + fNumber))
+       println("√($number1 * $number2 + $fNumber) = $answer")
+   }
+
+}
 
